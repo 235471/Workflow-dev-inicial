dc0146423a74ebfb3976e400031d0c6d157c59e7
Israel <ioliveirap90@gmail.com>
2025-02-09 20:57:29 -0300

    feat: :sparkles: add:methods to list books by author
    also includes unit test for model and integration test for controller

--- src/test/models/autor.test.js
+++ src/test/models/autor.test.js
@@ -57,4 +57,24 @@ describe('Testando o modelo Autor', () => {
       }),
     );
   });
+
+  it('Deve retornar uma lista de livros baseado no id do autor', async () => {
+    const listaLivros = await Autor.getLivroPorAutor(1);
+
+    expect(Array.isArray(listaLivros)).toBe(true);
+
+    listaLivros.forEach((livro) => {
+      expect(livro).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+          titulo: expect.any(String),
+          paginas: expect.any(Number),
+          editora_id: expect.any(Number),
+          autor_id: expect.any(Number),
+          created_at: expect.any(String),
+          updated_at: expect.any(String),
+        }),
+      );
+    });
+  });
 });
