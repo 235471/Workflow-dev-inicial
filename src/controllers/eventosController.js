2fc555a60878621b5688cd43b081aed5cd13b19a
Israel <ioliveirap90@gmail.com>
2025-02-10 19:14:30 -0300

    feat: :sparkles: add: feature flag para /eventos e testes unitarios

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,12 +1,18 @@
 import Evento from '../models/evento.js';
 
 class EventosController {
+  static grantEventoAcess = () => process.env.EVENTO_FLAG === 'true';
+
   static getEventos = async (_, res) => {
-    try {
-      const resultado = await Evento.getEventos();
-      return res.status(200).json(resultado);
-    } catch (err) {
-      return res.status(500).json(err.message);
+    if (this.grantEventoAcess()) {
+      try {
+        const resultado = await Evento.getEventos();
+        return res.status(200).json(resultado);
+      } catch (err) {
+        return res.status(500).json(err.message);
+      }
+    } else {
+      return res.status(404).send();
     }
   };
 }
