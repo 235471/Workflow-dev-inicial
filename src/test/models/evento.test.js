dd2a1d4da7ac3acc64a9902fe97bd90f4c5b6aa6
Israel <ioliveirap90@gmail.com>
2025-02-10 16:59:40 -0300

    feat: :sparkles: add: modelo eventos e teste do modelo

---
+++ src/test/models/evento.test.js
@@ -0,0 +1,21 @@
+import {
+  describe, expect, it,
+} from '@jest/globals';
+import Evento from '../../models/evento.js';
+
+describe('Testando o modelo Evento', () => {
+  const objetoEvento = {
+    nome: 'Evento teste',
+    descricao: 'Descrição teste',
+    data: '2025-02-10',
+    autor_id: 1,
+  };
+
+  it('Deve instanciar um novo evento', () => {
+    const evento = new Evento(objetoEvento);
+
+    expect(evento).toEqual(
+      expect.objectContaining(objetoEvento),
+    );
+  });
+});
